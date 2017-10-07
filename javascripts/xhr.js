972e179c1fb25ef304087a92a0ba8cf3cffcf00f
ben harrington <harringtonben@icloud.com>
2017-10-07 10:43:13 -0500

    badges printing to page

--- javascripts/xhr.js
+++ javascripts/xhr.js
@@ -2,11 +2,29 @@
 
 let badges = [];
 
-$.ajax("https://teamtreehouse.com/benharrington.json").done((data)=>{
-    badges = data.badges;
-    console.log(badges);
-}).fail((error)=>{
-    console.log(error);
-});
+    $.ajax("https://teamtreehouse.com/benharrington.json").done((data)=>{
+        badges = data.badges;
+        printDom(badges);
+    }).fail((error)=>{
+        console.log(error);
+    });
+
+const getBadges = () => {
+    return badges;
+};
+
+const printDom = (myBadges) => {
+    let printString = "";
+    myBadges.forEach((badge)=> {
+        printString += `<div class="badgeIcons col-md-3">
+                            <img src="${badge.icon_url}">
+                        </div>`;
+    });
+    printToPage(printString);
+};
+
+const printToPage = (strang) => {
+    $("#outputDiv").html(strang);
+};
 
 module.exports = {};
